b585102feb1b74746c77a73205c1dbdc244489b0
Johan Haleby <johan.haleby@gmail.com>
2019-02-27 14:48:17 +0100

    Only returning last 10 messages

--- src/main/kotlin/se/haleby/reactor/http/ChatController.kt
+++ src/main/kotlin/se/haleby/reactor/http/ChatController.kt
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.toFlux
 import se.haleby.reactor.Message
 import se.haleby.reactor.logging.loggerFor
 import se.haleby.reactor.mongo.ChatRepository
+import java.time.Duration
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 
@@ -26,6 +28,11 @@ class ChatController(private val chatRepository: ChatRepository) {
     @GetMapping
     fun streamMessages(): Flux<ServerSentEvent<HttpMessageDTO>> {
         return chatRepository.subscribeToMessages()
+                .bufferTimeout(1000, Duration.ofMillis(250))
+                .map { list ->
+                    list.takeLast(10)
+                }
+                .flatMap { list -> list.toFlux() }
                 .doOnSubscribe {
                     log.info("Client connected")
                 }
---
+++ src/main/kotlin/se/haleby/reactor/swearwords/swearwords.kt
@@ -0,0 +1,8 @@
+package se.haleby.reactor.swearwords
+
+//
+//val swearWords = arrayOf("Skånerost", "bar", "spam").joinToString(prefix = "(?i)", separator = "|").toRegex()
+//
+//fun replaceSwearWordsWithAsterisk(str: String) {
+//    swearWords.findAll(str).forEach { result.add(it.value) }
+//}
